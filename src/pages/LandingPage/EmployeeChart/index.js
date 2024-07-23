b824b96f4d380a1173a80f7c36419185eecc2a37
prakashraj950 <prakash950288@gmail.com>
2024-07-23 22:27:16 +0530

    build added

--- src/pages/LandingPage/EmployeeChart/index.js
+++ src/pages/LandingPage/EmployeeChart/index.js
@@ -3,61 +3,91 @@ import { Box } from '@mui/material';
 import Chart from 'react-apexcharts';
 
 const EmployeeChart = () => {
-  const options = {
-    chart: {
-      type: 'line',
-      height: 263,
-      toolbar: {
-        show: false
-      },
-    },
-    stroke: {
-      width: [0, 4]
-    },
-    // title: {
-    //   text: 'Monthly Data'
-    // },
-    dataLabels: {
-    //   enabled: true,
-      enabledOnSeries: [1]
-    },
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    xaxis: {
-      type: 'category'
-    },
-    yaxis: [
-      {
-        title: {
-        //   text: 'Bar Data',
+    const options = {
+        chart: {
+            type: 'line',
+            height: 263,
+
+            toolbar: {
+                show: false,
+                events: {
+                    render: () => {
+                        const bars = document.querySelectorAll('.apexcharts-bar-series .apexcharts-series-markers rect');
+                        bars.forEach(bar => {
+                            bar.style.borderTopLeftRadius = '4px';
+                            bar.style.borderTopRightRadius = '4px';
+                        });
+                    }
+                }
+
+
+            }
         },
-      },
-      {
-        opposite: true,
-        title: {
-        //   text: 'Line Data'
+        stroke: {
+            curve: 'smooth', 
+            width: [0, 4],
+            colors: ['#002B55BD'] 
+        },
+        // title: {
+        //   text: 'Monthly Data'
+        // },
+        dataLabels: {
+            //   enabled: true,
+            enabledOnSeries: [1]
+        },
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        xaxis: {
+            type: 'category'
+        },
+        // yaxis: [
+        //     {
+        //         title: {
+        //             //   text: 'Bar Data',
+        //         },
+        //     },
+        //     {
+        //         opposite: true,
+        //         title: {
+        //             //   text: 'Line Data'
+        //         }
+        //     }
+        // ],
+        plotOptions: {
+            bar: {
+                borderRadiusTop: 5,
+                colors: {
+                    ranges: [{
+                        from: 0,
+                        to: Infinity,
+                        color: '#277ACC'
+                    }]
+                }
+            }
+        },
+
+        legend: {
+            show: false  // Hide the legend
         }
-      }
-    ]
-  };
+    };
 
-  const series = [
-    {
-      name: 'Bar Data',
-      type: 'column',
-      data: [2000, 3000, 2000, 1500, 3500, 4000, 1000, 3000, 4500, 2500, 2000, 3500]
-    },
-    {
-      name: 'Line Data',
-      type: 'line',
-      data: [1200, 1400, 1000, 1500, 2100, 2000, 900, 1200, 2500, 1800, 1200, 1600]
-    }
-  ];
+    const series = [
+        {
+            name: 'Bar Data',
+            type: 'column',
+            data: [2000, 3000, 2000, 1500, 3500, 4000, 1000, 3000, 4500, 2500, 2000, 3500]
+        },
+        {
+            name: 'Line Data',
+            type: 'line',
+            data: [1200, 1400, 1000, 1500, 2100, 2000, 900, 1200, 2500, 1800, 1200, 1600]
+        }
+    ];
 
-  return (
-    <Box sx={{ p: 1, }}>
-      <Chart options={options} series={series} type="line" height={263} />
-    </Box>
-  );
+    return (
+        <Box sx={{ p: 1, }} >
+            <Chart options={options} series={series} type="line" height={263} />
+        </Box >
+    );
 };
 
 export default EmployeeChart;
